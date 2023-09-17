31ff9e5bf3f32a2fa245f9e83568fbad6b1dbf78
rudradip ray <317295@aaps.k12.mi.us>
2023-09-16 21:35:52 -0400

    navbar component active class js implementation

--- components/navbarC.js
+++ components/navbarC.js
@@ -4,6 +4,11 @@ class navbarC extends HTMLElement {
   }
   connectedCallback(){
     this.render()
+    const page = this.getAttribute('page');
+    console.log(page) //Returns "home"
+    console.log(typeof page) //Returns "string"
+    const change=this.ownerDocument.getElementById(page).classList.add("active");
+    console.log(change)
   }
   render(){
     this.innerHTML=`
@@ -34,40 +39,40 @@ class navbarC extends HTMLElement {
     color: #000000; 
 }
 </style>
-  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #fff;">
-    <div class="container-fluid">
-      <a class="navbar-brand px-3" href="#"> 
-        <img src="assets/logo2.png" width="30" height="30" alt="Game Day Grill">
-        Game Day Grill
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
-        aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbar">
-        <ul class="nav nav-pills ms-auto">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#" id="home">Home</a>
-          </li>
-          <li class="nav-item" id="menu">
-            <a class="nav-link" href="/menu.html">Menu</a>
-          </li>
-          <li class="nav-item" id="reservations">
-            <a class="nav-link" href="/reservation.html">Reservations</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/reviews.html" id="events">Reviews</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/aboutus.html" id="about-us">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/checkout.html" id="about-us">Checkout</a>
-          </li>
-        </ul>
-      </div>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #fff;">
+  <div class="container-fluid">
+    <a class="navbar-brand px-3" href="#"> 
+      <img src="assets/logo2.png" width="30" height="30" alt="Random Thingy">
+      Random Thingy
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
+      aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbar">
+      <ul class="nav nav-pills ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/index.html" id="home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/menu.html" id="menu">Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/reservation.html" id="reservations">Reservations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/reviews.html" id="reviews">Reviews</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/aboutus.html" id="about-us">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/checkout.html" id="checkout">Checkout</a>
+        </li>
+      </ul>
     </div>
-  </nav>`
+  </div>
+</nav>`
   }
 }
 
