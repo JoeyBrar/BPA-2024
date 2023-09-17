55505a08e52eec99acaf31570a7ce18638a8911a
rudradip ray <317295@aaps.k12.mi.us>
2023-09-16 22:33:27 -0400

    footer info additions

--- components/footerC.js
+++ components/footerC.js
@@ -7,9 +7,18 @@ class footerC extends HTMLElement {
   }
   render(){
     this.innerHTML=`
+<style>
+.multiline {
+  white-space: pre-wrap;
+}
+</style>
 <div class="container align-items-center">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p class="col-md-4 mb-0 text-body-secondary text-center justify-content-center mx-auto">© 2023 Game Day Grill, Inc</p>
+    <p class="col-md-4 mb-0 text-body-secondary text-center justify-content-center mx-auto multiline">
+    123 Easy St. Funland, Michigan. 12345
+    11:30am-9pm M-F, Noon-9pm S-S
+    (987)-654-3210
+    </p>
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
       <img src="/assets/logo.png" alt="Game Day Grill" style="max-width:50%">
     </a>
