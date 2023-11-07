0b24064d7be77a8245f399f7f63a4fd91a8aba8d
rudradip ray <317295@aaps.k12.mi.us>
2023-11-07 12:48:40 -0500

    fixed net total calculation errors

--- js/items.js
+++ js/items.js
@@ -9,9 +9,10 @@ function printAll () {
 }
 
 async function finalPrice () {
-  a = await salesTax();
+  //a = await salesTax();
+  //console.log(typeof(a))
   b = await grabTotal();
-  return Number(a)+Number(b);
+  return ((b*1.06).toFixed(2));
 }
 
 
@@ -108,3 +109,30 @@ makeElement ("The Classic Burger", 3)
 makeElement("Buffalo Wings", 2)
 editElement ("Grilled Chicken Club", 6)
 deleteElement("The Classic Burger")
+
+async function inputFinalPrice() {
+  let finalTotal = await finalPrice()
+  document.getElementById("total-price").innerHTML=`$${finalTotal}`
+}
+
+async function inputSalesTax() {
+  let salesTaxFinal = await salesTax()
+  document.getElementById("sales-tax").innerHTML=`$${salesTaxFinal}`
+}
+
+async function inputCheckoutItems () {
+  var keyValuePairs = document.cookie.split(';');
+  const ins = document.getElementById("insert-items");
+  for(var i = 0; i < keyValuePairs.length; i++) {
+      let value = keyValuePairs[i].substring(keyValuePairs[i].indexOf('=')+1).trim();
+      let item = document.createElement('checkout-item-c');
+      item.setAttribute("name", value.split('+')[0].trim());
+      item.setAttribute("price", await grabPrice(value.split('+')[0].trim()))
+      item.setAttribute("qty", Number(value.split('+')[1].trim()))
+      ins.insertAdjacentElement("afterend",item);
+  }
+}
+
+inputFinalPrice()
+inputSalesTax()
+inputCheckoutItems()
