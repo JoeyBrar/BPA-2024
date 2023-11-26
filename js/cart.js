1ac71c4ee837d9388c35e3368368d391813fbc6f
rudradip ray <317295@aaps.k12.mi.us>
2023-11-25 19:29:01 -0500

    added cart delete feature

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,45 @@
+async function loopFood () {
+  var keyValuePairs = document.cookie.split(';');
+  for(var i = 0; i < keyValuePairs.length; i++) {
+      var c2 = keyValuePairs[i].substring(keyValuePairs[i].indexOf('=')+1).trim();
+      var name = c2.split('+')[0].trim();
+      await insertFoodS(name);
+  }
+}
+
+async function insertFoodS(food) {
+  var foodName = food;
+  var foodDesc = await grabDesc(food);
+  var foodPrice = await grabPrice(food);
+  var foodQty = await grabQuantity(food);
+  var table = document.getElementById("shoppingCart");
+  var row = table.insertRow(0);
+  var nameAndDesc = row.insertCell(0);
+  var price = row.insertCell(1);
+  var qty = row.insertCell(2);
+  var deleteBtn = row.insertCell(3);
+  nameAndDesc.innerHTML = `
+  <div class="row">
+    <div class="col-md-12 text-left mt-sm-2">
+        <h4 id="foodC">${foodName}</h4>
+        <p class="font-weight-light">${foodDesc}</p>
+    </div>
+  </div>
+  `;
+  price.innerHTML = `$${foodPrice}`;
+  price.classList.add("align-middle");
+  qty.innerHTML = `
+  <input type="number" class="form-control form-control-lg text-center align-middle" value="${foodQty}" min="1">
+  `;
+  qty.classList.add("align-middle");
+  deleteBtn.innerHTML = `
+  <div class="text-center">
+    <button type="button" class="btn-close align-middle" aria-label="Close" onclick="deleteRow(this,'${foodName}')"></button>
+  </div>
+  `;
+  deleteBtn.classList.add("align-middle");
+}
+
 async function grabTotal () {
   var keyValuePairs = document.cookie.split(';');
   let total = 0
@@ -42,14 +84,58 @@ function editElement (name, quantityE) {
   document.cookie = `${cf1}=${name}+${quantityE}`;
 }
 
+async function grabPrice (name) {
+  const response = await fetch ('/assets/menu/whole.json');
+  const items = await response.json();
+  for (let i=0;i<items.length;i++) {
+    let obj = items[i];
+    if (obj.name == name) {
+      //console.log(obj.price)
+      return obj.price
+    }
+  }
+}
+
+async function grabDesc (name) {
+  const response = await fetch ('/assets/menu/whole.json');
+  const items = await response.json();
+  for (let i=0;i<items.length;i++) {
+    let obj = items[i];
+    if (obj.name == name) {
+      //console.log(obj.price)
+      return obj.description;
+    }
+  }
+}
+
 async function grabQuantity (name) {
   var keyValuePairs = document.cookie.split(';');
   for(var i = 0; i < keyValuePairs.length; i++) {
       var c1 = keyValuePairs[i].substring(0, keyValuePairs[i].indexOf('=')).trim();
       var c2 = keyValuePairs[i].substring(keyValuePairs[i].indexOf('=')+1).trim();
-      console.log(c2.split('+')[1], "grabQuantity")
       if (c2.split('+')[0].trim() == name) {
         return Number(c2.split('+')[1])
       }
   }
+}
+
+async function deleteRow(r, name) {
+  var i = r.parentNode.parentNode.rowIndex;
+  var tbl = document.getElementById("shoppingCart");
+  document.getElementById("shoppingCart").deleteRow(i);
+  await deleteElement(name)
+}
+
+async function grabTotal () {
+  var keyValuePairs = document.cookie.split(';');
+  let total = 0
+  for(var i = 0; i < keyValuePairs.length; i++) {
+      var name = keyValuePairs[i].substring(0, keyValuePairs[i].indexOf('=')).trim();
+      var value = keyValuePairs[i].substring(keyValuePairs[i].indexOf('=')+1).trim();
+      a = await grabPrice(value.split('+')[0]);
+      b = await value.split('+')[1];
+      total+=a*b
+  }
+  //console.log(total)
+  return await total
 }
